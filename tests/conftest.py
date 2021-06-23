b5d484c091bf1c622d4ba52fe69f1611ede7ef24
Xavier Beheydt <xavier.beheydt@gmail.com>
2021-06-23 16:48:34 +0200

    Fix tmp_path woth explicit str convertion

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ def cli(monkeypatch, tmp_path):
     """
     Make a runner for cli entrepoint.
     """
-    monkeypatch.setenv("HOME", tmp_path, prepend=False)
+    monkeypatch.setenv("HOME", str(tmp_path), prepend=False)
     b_dir = tmp_path / ".blenderenv"
     b_dir.mkdir()
     yield CliRunner()
