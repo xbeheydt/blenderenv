852630384c77831e2f3ab67edf3df9d9675c9b5a
Xavier Beheydt <xavier.beheydt@gmail.com>
2021-06-23 18:47:30 +0200

    fix imports

--- src/cli/commands/__init__.py
+++ src/cli/commands/__init__.py
@@ -2,7 +2,12 @@
 Sub module commands
 """
 
-from . import _global, _install, _local, _uninstall, _version, _versions
+from cli.commands._global import _global
+from cli.commands._install import _install
+from cli.commands._local import _local
+from cli.commands._uninstall import _uninstall
+from cli.commands._version import _version
+from cli.commands._versions import _versions
 
 
 __all__ = [
