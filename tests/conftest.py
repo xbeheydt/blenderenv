40948e497dd5ddac935ee56520823fffa32139c6
Xavier Beheydt <xavier.beheydt@gmail.com>
2021-07-02 13:07:39 +0200

    Build new conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,44 @@
 Configuration for pytest
 """
 
+import os
+
 import pytest
 from click.testing import CliRunner
+from pytest import TempPathFactory
+
+
+# region Class Tools
+class AppSetup:
+    """
+    AppSetup configures app environment in temporary path.
+    """
+
+    def __init__(self, tmp_path_factory: TempPathFactory):
+        self.tmp_path = tmp_path_factory.mktemp("test")
+
+        self.blenderenv_home = self.tmp_path / ".blenderenv"
+        self.blenderenv_home.mkdir()
+        os.environ["BLENDERENV_HOME"] = str(self.blenderenv_home)
+
+        self.shims = self.blenderenv_home / "shims"
+        self.shims.mkdir()
+        self.cache = self.blenderenv_home / "cache"
+        self.cache.mkdir()
+        self.bin = self.blenderenv_home / "bin"
+        self.bin.mkdir()
+        self.versions = self.blenderenv_home / "versions"
+        self.versions.mkdir()
+
+        self.lib = self.blenderenv_home / "lib"
+        self.lib.mkdir()
+        self.vendor = self.lib / "_vendor"
+        self.vendor.mkdir()
+
+        from blenderenv.config import Config
+
+        self.config = Config()
+        self.config.init_default()
 
 
 class CustomCliRunner(CliRunner):
@@ -11,32 +47,27 @@ class CustomCliRunner(CliRunner):
     Custom cli runner which prepare project folder test.
     """
 
-    def __init__(self, monkeypatch, tmp_path):
-        monkeypatch.setenv("HOME", str(tmp_path), prepend=False)
-
-        self.tmp_path = tmp_path
-
-        self.root_dir = self.tmp_path / ".blenderenv"
-        self.root_dir.mkdir()
-
-        self.shims = self.root_dir / "shims"
-        self.shims.mkdir()
-
-        self.bins = self.root_dir / "bin"
-        self.bins.mkdir()
-
-        self.versions = self.root_dir / "versions"
-        self.versions.mkdir()
-
-        self.version = self.root_dir / "version"
-        self.version.write_text("")
-
+    def __init__(self, tmp_path):
         super().__init__()
 
 
+# endregion
+
+# region Fixtures
+@pytest.fixture(scope="session", autouse=True)
+def app(tmp_path_factory):
+    """
+    Configure app application for tests.
+    """
+    yield AppSetup(tmp_path_factory)
+
+
 @pytest.fixture
-def app(monkeypatch, tmp_path):
+def cli():
     """
     App object helps to run tests.
     """
-    yield CustomCliRunner(monkeypatch, tmp_path)
+    yield CustomCliRunner()
+
+
+# endregion
